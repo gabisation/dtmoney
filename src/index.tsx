799057b424712d4d491b1978f481547d9c0ed8c0
Gabrielly Barros Fonseca <gabrielly.barros@hotmail.com>
2022-01-28 20:18:21 -0300

    Listing transactions and seeds

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,42 @@ createServer({
     transaction: Model
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-15 11:00:00')
+        },
+        {
+          id: 3,
+          title: 'Internet',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 120,
+          createdAt: new Date('2021-02-20 10:00:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return this.schema.all('transactions');
+      return this.schema.all('transaction');
     })
 
     this.post('/transactions', (schema, request) => {
