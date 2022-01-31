9352e6904c417e2e99f4e13709085c70691a0b8f
Gabrielly Barros Fonseca <gabrielly.barros@hotmail.com>
2022-01-31 08:32:28 -0300

    Loading transactions

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,23 +1,9 @@
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
+import { useContext } from "react";
+import { TransactionsContext } from "../../TransactionsContext";
 import { Container } from "./styles";
 
-interface Transaction {
-  id: number;
-  title: string;
-  type: string;
-  category: string;
-  amount: number;
-  createdAt: string;
-}
-
 export function TransactionsTable() {
-  const [transactions, setTransactions] = useState<Transaction[]>([])
-
-  useEffect(() => {
-    api.get('transactions')
-      .then(response => setTransactions(response.data.transactions))
-  }, []);
+  const transactions = useContext(TransactionsContext);
 
   return (
     <Container>
@@ -32,7 +18,7 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          {transactions.map((transaction: Transaction)  => (
+          {transactions.map(transaction  => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
               <td className={transaction.type}>
